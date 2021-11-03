a315dde373dbb28214ecbfa5ddba418a37f8a4c5
Fagner Miranda Caldas <fagner.miranda01@gmail.com>
2021-11-03 12:05:03 -0300

    Curso Formação Apache Kafka Alura

---
+++ ecommerce/service-new-order/src/main/java/br/com/alura/ecommerce/NewOrderMain.java
@@ -0,0 +1,26 @@
+package br.com.alura.ecommerce;
+
+import br.com.alura.ecommerce.dispatcher.KafkaDispatcher;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            for (var i = 0; i < 10; i++) {
+                var orderId = UUID.randomUUID().toString();
+                var amount = BigDecimal.valueOf(Math.random() * 5000 + 1);
+                var email = Math.random() + "email.com";
+
+                var order = new Order(orderId, amount, email);
+                orderDispatcher.send("ECOMMERCE_NEW_ORDER", email,
+                        new CorrelationId(NewOrderMain.class.getSimpleName()),
+                        order);
+            }
+        }
+    }
+
+}
